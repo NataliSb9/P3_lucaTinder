abd28c0efa8f9165aefa366f76ca973ce30f9218
NataliSb9 <nsainzbarroso@gmail.com>
2021-11-30 18:36:39 +0100

    api inicializada con un get de todos los usuarios

--- servicio/routes/usuario.routes.js
+++ servicio/routes/usuario.routes.js
@@ -1,12 +1,27 @@
-
-
 const express = require('express');
 //Importo la función de express que hace posible crear una aplicación express
 const app = express();
 
-//importo el schema de la BBDD
-let usuario = require('../model/schemaUsuario.js')
-
 // route() miniaplicación”, capaz solo de realizar funciones de middleware y enrutamiento. Cada aplicación Express tiene un enrutador de aplicaciones incorporado.
 //  Router() method that creates a new router object.
-const usuarioRoute = express.Router()
+const usuarioRoute = express.Router();
+
+//importo el schema de la BBDD
+
+let usuario = require('../model/schemaUsuario.js');
+
+//En este punto se hacen las peticiones
+
+//mostrar todos los usuarios:
+
+usuarioRoute.route('/').get((req,res)=>{
+    usuario.find((error,data)=>{
+        if(error) {
+            return next(error);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+module.exports = usuarioRoute;
