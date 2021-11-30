e768c09bd9d544caf235e50486578fa615a3b127
NataliSb9 <nsainzbarroso@gmail.com>
2021-11-30 15:45:25 +0100

    agregado express en index

--- servicio/index.js
+++ servicio/index.js
@@ -1,2 +1,22 @@
 //Aquí es donde confluye la información de los otros documentos.
 //Aquí es donde se indican los Midleware.
+//cors hacer un npm de cors
+let express = require('express'),
+    path = require('path'),
+    mongoose = require('mongoose'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    mongoDb = //PONER LA BBDD;
+//mongoDb => es la BBDD.    
+mongoose.Promise = global.Promise;
+
+mongoose.connect(mongoDb.db, {useNewUrlParser: true, useUnifiedTopology: true},
+    function(error){
+        if(error){
+            throw error;
+        }else{
+            console.log('Te has conectado con éxito a la base de datos')
+        }
+    }
+)
+
--- servicio/routes/usuario.routes.js
+++ servicio/routes/usuario.routes.js
@@ -1,4 +1,12 @@
-//Todo lo relativo a los endPoints del usuario van aquí.
-//Importante! comentar lo que hace cada endpoint de forma clara.
-//Aquí es donde se importa la conexión con la BD.
-//Aquí es donde se importar mongoose.
+
+
+const express = require('express');
+//Importo la función de express que hace posible crear una aplicación express
+const app = express();
+
+//importo el schema de la BBDD
+let usuario = require('../model/schemaUsuario.js')
+
+// route() miniaplicación”, capaz solo de realizar funciones de middleware y enrutamiento. Cada aplicación Express tiene un enrutador de aplicaciones incorporado.
+//  Router() method that creates a new router object.
+const usuarioRoute = express.Router()
