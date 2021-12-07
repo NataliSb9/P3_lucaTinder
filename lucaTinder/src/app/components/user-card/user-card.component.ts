ab38d6990cb746492f9dc7e348e736b8094c531b
Sergio Martínez <sergiotr94@gmail.com>
2021-12-07 17:26:50 +0100

    Cambios género

--- lucaTinder/src/app/components/user-card/user-card.component.ts
+++ lucaTinder/src/app/components/user-card/user-card.component.ts
@@ -1,8 +1,13 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit, Input } from '@angular/core';
 import { UsuarioMatch } from 'src/app/models/usuarioMatch.model';
 import { UsuarioService } from '../../services/usuario.service';
-
 
 @Component({
   selector: 'app-user-card',
@@ -10,63 +15,81 @@ import { UsuarioService } from '../../services/usuario.service';
   styleUrls: ['./user-card.component.scss'],
   animations: [
     trigger('flipState', [
-      state('active', style({
-        transform: 'rotateY(179deg)'
-      })),
-      state('inactive', style({
-        transform: 'rotateY(0)'
-      })),
+      state(
+        'active',
+        style({
+          transform: 'rotateY(179deg)',
+        })
+      ),
+      state(
+        'inactive',
+        style({
+          transform: 'rotateY(0)',
+        })
+      ),
       transition('active => inactive', animate('500ms ease-out')),
-      transition('inactive => active', animate('500ms ease-in'))
-    ])
-  ]
+      transition('inactive => active', animate('500ms ease-in')),
+    ]),
+  ],
 })
 export class UserCardComponent implements OnInit {
-  @Input() usuario:any; 
-  @Input() btnMostrador:any;
-  public usuarioExportable!:any
+  @Input() usuario: any;
+  @Input() btnMostrador: any;
+  public usuarioExportable!: any;
   constructor(private usuarioService: UsuarioService) {}
 
-// redirectToInfo(){
-//   window.location.href = './home/profileInfo/infoDeployed';
-//localStorage.setItem('perfilVisitable', this.usuario.email);
-// }
+  // redirectToInfo(){
+  //   window.location.href = './home/profileInfo/infoDeployed';
+  //localStorage.setItem('perfilVisitable', this.usuario.email);
+  // }
 
-
-
- ngOnInit(): void {
-   this.usuarioExportable=this.usuario 
-   
+  ngOnInit(): void {
+    this.usuarioExportable = this.usuario;
   }
 
   flip: string = 'inactive';
 
   toggleFlip() {
-    this.flip = (this.flip == 'inactive') ? 'active' : 'inactive';
+    this.flip = this.flip == 'inactive' ? 'active' : 'inactive';
   }
 
-  getUrl(){
-   
-    return "url("+this.usuario.foto+")";
+  getUrl() {
+    return 'url(' + this.usuario.foto + ')';
   }
 
-  //debo incluir aqui el email 
+  //debo incluir aqui el email
   email: string = '';
   myEmail: any = window.localStorage.getItem('usuarioActual');
   checkIfIsLike: boolean = true;
 
   pulsaLikeDislike(checkIfIsLike: boolean, emailLikeDislike: string) {
     let myUsuario: UsuarioMatch = new UsuarioMatch(
-      this.email = emailLikeDislike,
+      (this.email = emailLikeDislike),
       this.myEmail,
       checkIfIsLike
     );
 
-    this.usuarioService.addLikeOrDislike(myUsuario).subscribe((data) => {
-     
-    });
+    this.usuarioService.addLikeOrDislike(myUsuario).subscribe((data) => {});
 
     // Ayudante para Test
     this.checkIfIsLike = checkIfIsLike;
+  }
+
+  getUrlSex() {
+    if (this.usuario.genero == 'male' && this.usuario.prefGen == 'male') {
+      return 'url(../../../assets/imgs/mars-double-solid.svg)';
+    } else if (
+      (this.usuario.genero == 'male' && this.usuario.prefGen == 'female') ||
+      (this.usuario.genero == 'female' && this.usuario.prefGen == 'male')
+    ) {
+      return 'url(../../../assets/imgs/venus-mars-solid.svg)';
+    } else if (
+      this.usuario.genero == 'female' &&
+      this.usuario.prefGen == 'female'
+    ) {
+      return 'url(../../../assets/imgs/venus-double-solid.svg)';
+    } else {
+      return 'url(../../../assets/imgs/venus-mars-solid.svg)';
+    }
   }
 }
