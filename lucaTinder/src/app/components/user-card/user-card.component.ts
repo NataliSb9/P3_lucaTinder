b674f80b53afe1b409ca3b8e8b2762ee2c7e9a3e
KamiSama <97alexisbermudez97@gmail.com>
2021-12-07 12:15:59 +0100

    cards in final version

--- lucaTinder/src/app/components/user-card/user-card.component.ts
+++ lucaTinder/src/app/components/user-card/user-card.component.ts
@@ -1,22 +1,47 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit, Input } from '@angular/core';
 
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.scss']
+  styleUrls: ['./user-card.component.scss'],
+  animations: [
+    trigger('flipState', [
+      state('active', style({
+        transform: 'rotateY(179deg)'
+      })),
+      state('inactive', style({
+        transform: 'rotateY(0)'
+      })),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class UserCardComponent implements OnInit {
   @Input() usuario:any; 
   @Input() btnMostrador:any;
   constructor() { }
 
-redirectToInfo(){
-  window.location.href = './home/profileInfo/infoDeployed';
-}
+// redirectToInfo(){
+//   window.location.href = './home/profileInfo/infoDeployed';
+// }
+
 
 
   ngOnInit(): void {
+  }
+
+  flip: string = 'inactive';
+
+  toggleFlip() {
+    this.flip = (this.flip == 'inactive') ? 'active' : 'inactive';
+  }
+
+  getUrl(){
+    console.log("foto: "+this.usuario.foto);
+    return "url("+this.usuario.foto+")";
   }
 
 }
